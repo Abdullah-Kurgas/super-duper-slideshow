3f73f4cb5b4720d5e4db57d2589125fc4d0710bb
abdullah.kurgas <abdullah.kurgas@morescreens.net>
2022-06-08 15:57:01 +0200

    Fix environment ytBaseUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   backendUrl: 'https://super-duper-slideshow-back.herokuapp.com/',
   cloudName: 'dzbizu8pe',
   uploadPreset: 'awhrycmx',
-  ytBaseUrl: 'https://www.googleapis.com/youtube/v3',
+  ytBaseUrl: 'https://www.googleapis.com/youtube/v3/',
   ytApiKey:'AIzaSyACrIDpKE-sa1AOeTnreUuhwWWFGarw-QU'
 };
 
