a3a714cd48aebea6d68d96870f121c39a588bcd3
abdullah.kurgas <abdullah.kurgas@morescreens.net>
2022-06-09 12:07:11 +0200

    Change side-bar style

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/app/shared/models/User';
+import { ApiService } from 'src/app/shared/services/api.service';
 import { LoaderService } from 'src/app/shared/services/loader.service';
 import { UserService } from 'src/app/shared/services/user.service';
 import Utils from 'src/app/shared/Utils';
@@ -18,36 +19,50 @@ export class LoginComponent implements OnInit {
   errMessage!: string;
 
   constructor(
+    public loaderService: LoaderService,
     private userService: UserService,
-     private router: Router,
-     public loaderService: LoaderService
-     ) { }
+    private apiService: ApiService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-    setTimeout(()=>{
+    setTimeout(() => {
       this.loaderService.hideFullScreenLoading();
     })
-   }
+  }
 
   executeLogin() {
     this.isLoading = true;
 
-    this.userService.executeLogin(this.user).subscribe((response: any) => {
-      if (!response.id) {
-        this.errMessage = response.msg;
-        this.isLoading = false;
-        return
-      };
+    this.userService.executeLogin(this.user).subscribe({
+      next: (res: any) => {
+        if (!res.id) {
+          this.errMessage = res.msg;
+          this.isLoading = false;
+          return
+        };
 
-      if (this.isCheckBoxChecked) {
-        localStorage.setItem('userData', JSON.stringify(response));
-      } else {
-        sessionStorage.setItem('userData', JSON.stringify(response));
-      }
-      this.router.navigate(['dashboard']).then(()=>{
-        this.userService.userData = Utils.getDataFromLocalOrSession('userData');
+        if (this.isCheckBoxChecked) {
+          localStorage.setItem('userData', JSON.stringify(res));
+        } else {
+          sessionStorage.setItem('userData', JSON.stringify(res));
+        }
+        this.router.navigate(['dashboard']).then(() => {
+          this.userService.userData = Utils.getDataFromLocalOrSession('userData');
+          this.isLoading = false;
+        });
+      },
+      error: (err: Error) => {
         this.isLoading = false;
-      });
+        this.apiService.showToasrtMsg('error', err.message);
+
+        this.apiService.showModal('serverError').afterClosed()
+          .subscribe(() => {
+            this.apiService.showToasrtMsg('success', 'Server has been successfully restarted');
+            this.loaderService.hideLoading();
+            location.reload();
+          });
+      }
     })
   }
 }
