4b5c7710b2a73701bfd61cbde86ef62346f8f81c
abdullah.kurgas <abdullah.kurgas@morescreens.net>
2022-06-09 15:32:28 +0200

    Create emailValidity

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from 'src/app/shared/models/User';
+import { ApiService } from 'src/app/shared/services/api.service';
 import { LoaderService } from 'src/app/shared/services/loader.service';
 import { UserService } from 'src/app/shared/services/user.service';
 
@@ -14,8 +16,10 @@ export class SignUpComponent implements OnInit {
   isLoading: boolean = false;
 
   constructor(
+    private loaderService: LoaderService,
     private userService: UserService,
-    private loaderService: LoaderService
+    private apiService: ApiService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -27,15 +31,26 @@ export class SignUpComponent implements OnInit {
   executeSignUp() {
 
     this.userService.executeSignUp(this.user).subscribe({
-      next: (response) => {
-        console.log(response);
-        
+      next: (res: any) => {
+        this.apiService.showToasrtMsg('success', res.msg);
+        this.router.navigate(['login']);
       },
-      error: () => {
-
+      error: (err: Error) => {
+        this.apiService.showToasrtMsg('error', err.message);
       }
     }
     )
+  }
+
+  checkEmailValidity() {
+    if (!this.user.email) return true;
+    let crazyA = this.user.email.includes('@');
+    let dotAfterA = this.user.email.substring(this.user.email.indexOf('@')).includes('.');
+    let lastDot = this.user.email.split('').reverse().join('').substring(0, this.user.email.split('').reverse().join('').indexOf('.')).length > 1;
+
+    if (!crazyA || !dotAfterA) return false;
+    if (crazyA && dotAfterA) if (!lastDot) return false;
+    return true;
   }
 
 }
