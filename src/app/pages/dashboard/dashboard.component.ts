19f91840b499af99bdd21aa5c4fba46d147bc247
abdullah.kurgas <abdullah.kurgas@morescreens.net>
2022-06-13 09:02:08 +0200

    Add menu modal for user avatar

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { User } from 'src/app/shared/models/User';
 import { LoaderService } from 'src/app/shared/services/loader.service';
 import { UserService } from 'src/app/shared/services/user.service';
 
@@ -10,12 +9,25 @@ import { UserService } from 'src/app/shared/services/user.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  isUserMenuOpened: boolean = false;
 
   constructor(public router: Router, public userService: UserService, private loaderService: LoaderService) { }
 
   ngOnInit(): void {
     if (this.router.url === '/dashboard')
       this.router.navigate(['dashboard/create']);
+  }
+
+  openUserMenu(e: Event, page?: string) {
+    if (page === 'close') {
+      this.isUserMenuOpened = false;
+    } else if (page === 'stay') {
+      this.isUserMenuOpened = true;
+      e.stopPropagation();
+    } else {
+      this.isUserMenuOpened = !this.isUserMenuOpened;
+      e.stopPropagation();
+    }
   }
 
   logout() {
