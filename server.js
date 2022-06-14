e93df6b43f468a240184350cce6bff77019d0342
abdullah.kurgas <abdullah.kurgas@morescreens.net>
2022-06-14 13:51:42 +0200

    Trying deploy using dist

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/super-duper-slideshow'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: './src/' }),
+    res.sendFile('index.html', { root: '/dist/super-duper-slideshow/' }),
 );
 
 // Start the app by listening on the default Heroku port
